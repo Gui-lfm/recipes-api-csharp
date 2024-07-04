b1f58d6dbc3d23b3827f0eb2e0f3f5f0e76ff2b3
Guilherme Lucena <guilherme.lucena17@gmail.com>
2024-07-04 20:43:40 -0300

    feat(UserController.cs): req08
    Rota /PUT user/{email} implementada

--- src/recipes-api/Controllers/UserController.cs
+++ src/recipes-api/Controllers/UserController.cs
@@ -43,11 +43,24 @@ public class UserController : ControllerBase
         return CreatedAtAction("Get", new { email = user.Email }, user);
     }
 
-    // "8 - Sua aplicação deve ter o endpoint PUT /user
+    // PUT /user
     [HttpPut("{email}")]
     public IActionResult Update(string email, [FromBody] User user)
     {
-        throw new NotImplementedException();
+        bool exists = _service.UserExists(email);
+
+        if (!exists)
+        {
+            return NotFound();
+        }
+
+        if (email.ToLower() != user.Email.ToLower())
+        {
+            return BadRequest();
+        }
+
+        _service.UpdateUser(user);
+        return Ok(user);
     }
 
     // 9 - Sua aplicação deve ter o endpoint DEL /user
